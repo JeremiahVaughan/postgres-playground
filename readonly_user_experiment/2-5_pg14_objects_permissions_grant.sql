48f46744c88a331b5f6be383df232e09b1053e59
Vaughan, Jeremiah <jeremiah.vaughan@ruckuswireless.com>
2022-10-17 11:45:13 -0400

    Added experiment flow for p14 pg_read_all_data use case

---
+++ readonly_user_experiment/2-5_pg14_objects_permissions_grant.sql
@@ -0,0 +1,2 @@
+-- Run as postgres user
+GRANT pg_read_all_data TO bagger;
---
+++ readonly_user_experiment/3_create_additional_objects.sql
@@ -0,0 +1,11 @@
+-- Run this query as user 'owner' in the database 'store_1'
+
+-- DDL
+create table public.deli_order
+(
+    id integer
+);
+
+-- DML
+INSERT INTO public.deli_order (id)
+VALUES (0);
